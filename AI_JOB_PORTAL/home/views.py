7ac2df8672ec639ee1600812b0980d53e59a24c9
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-08-09 23:33:04 +0530

    extracted all details from resume

--- AI_JOB_PORTAL/home/views.py
+++ AI_JOB_PORTAL/home/views.py
@@ -14,12 +14,18 @@ import os
 from werkzeug.utils import secure_filename
 import PyPDF2
 import re
+import spacy
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 # Create your views here.
 
 cred = credentials.Certificate("/home/mohammed_shaneeb/Desktop/AI-JOB-PORTAL/firebasecrediantials.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "resume-qa-4de39.appspot.com"})
 bitly_shortener = pyshorteners.Shortener(api_key="1e11476d96a20bbd9924ad6ced6ba426f90039da",timeout=10)
 api_key = 'JuFDoAhCjQj9XnLZKiJ7VwHMpoaWGUbYRNBNmLATQPQOfx2f4fnmFyT7Viqi'
+
+nlp = spacy.load('/home/mohammed_shaneeb/Desktop/AI-JOB-PORTAL/training_Custom_NER/output/model-best')
 
 @login_required(login_url="signin")
 def home(request):
@@ -69,6 +75,24 @@ def submit_job(request,job_id):
             for chunk in pdf_file.chunks():
                 destination.write(chunk)
 
+        # Making Fire base link
+        pdf_file.seek(0)
+
+        bucket = storage.bucket()
+        blob = bucket.blob(pdf_file.name)
+        blob.upload_from_file(pdf_file)
+
+        expiration = datetime.utcnow() + timedelta(days=30)
+
+        # Get the download URL (link) of the uploaded file
+        download_url = blob.generate_signed_url(expiration)
+        print(download_url)
+        long_url = download_url
+        api_url = f'http://tinyurl.com/api-create.php?url={long_url}&apikey={api_key}'
+        response = requests.get(api_url)
+        short_url = response.text
+        print(short_url)
+
         resume_content = extract_text_from_pdf(file_path)
         job_description = re.sub(r'\s+|[•&]', ' ', job_description).lower()
         job_description = job_description.replace("\\r\\n", "")
@@ -80,6 +104,103 @@ def submit_job(request,job_id):
         print('\n')
         print("Job Description")
         print(job_description)
+
+        def unique_skills(doc):
+            skills = []
+            unique_skills = []
+            for ent in doc.ents:
+                if ent.label_ == "SKILL" or ent.label_ == "LANGUAGE":
+                    skills.append(ent.text)
+                    unique_skills = list(set(skills))
+            return unique_skills
+        
+        def get_name(doc):
+            name = ""
+            for ent in doc.ents:
+                if ent.label_ == "NAME":
+                    name = ent.text
+            return name
+        
+        def get_email(doc):
+            email = ""
+            for ent in doc.ents:
+                if ent.label_ == "EMAIL":
+                    email = ent.text
+            return email
+        
+        def get_phone(doc):
+            phone = ""
+            for ent in doc.ents:
+                if ent.label_ == "PHONE":
+                    phone = ent.text
+            return phone
+        
+        def get_education(doc):
+            education = ""
+            for ent in doc.ents:
+                if ent.label_ == "EDUCATION":
+                    education = ent.text
+            return education
+        def get_place(doc):
+            place = ""
+            for ent in doc.ents:
+                if ent.label_ == "PLACE":
+                    place = ent.text
+            return place
+        
+        def similarity_score(resume_skills,job_description_skills):
+            
+            # Combine both lists to create a corpus
+            corpus = resume_skills + job_description_skills
+
+            # Initialize the TF-IDF vectorizer
+            vectorizer = TfidfVectorizer()
+
+            # Compute the TF-IDF vectors for the corpus
+            tfidf_matrix = vectorizer.fit_transform(corpus)
+
+            # Separate the TF-IDF vectors for resume and job description skills
+            resume_tfidf = tfidf_matrix[:len(resume_skills)]
+            job_description_tfidf = tfidf_matrix[len(resume_skills):]
+
+
+            # Compute the Cosine similarity between resume and job description skills
+            cosine_similarities = cosine_similarity(resume_tfidf, job_description_tfidf)
+
+            # Get the similarity score for each skill in the resume with respect to job description skills
+            resume_scores = cosine_similarities.max(axis=1)
+
+            # Calculate the average similarity score for the entire resume
+            resume_similarity_score = np.mean(resume_scores)
+
+            return resume_similarity_score
+        
+        doc_resume = nlp(resume_content)
+        doc_job = nlp(job_description)
+
+            
+        resume_skills = unique_skills(doc_resume)
+        job_skills = unique_skills(doc_job)
+
+        name = get_name(doc_resume)
+        email = get_email(doc_resume)
+        place = get_place(doc_resume)
+        phone = get_phone(doc_resume)
+        education = get_education(doc_resume)
+
+
+        print(resume_skills)
+        print(job_skills)
+        print("Name :",name,"Email :",email,"Phone :",phone, "Education :",education, "Place :",place)
+
+
+        resume_score = similarity_score(resume_skills,job_skills)
+        resume_score = round((((resume_score + 1) / 2) * 9 + 1),1)
+
+        print(resume_score)
+
+        #resume_skills converting to comma seperated string to save in mysql database
+        resume_skills  = ','.join(map(str, resume_skills))
 
        
 
