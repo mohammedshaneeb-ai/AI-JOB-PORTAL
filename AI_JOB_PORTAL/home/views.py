03ce9caebb6f371318148a546127fe61d08cdb4d
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-08-09 18:21:59 +0530

    resume upload,extraction completed

--- AI_JOB_PORTAL/home/views.py
+++ AI_JOB_PORTAL/home/views.py
@@ -4,17 +4,22 @@ from admins.models import Job
 from markupsafe import Markup
 from django.utils.safestring import mark_safe
 from django.template.defaultfilters import linebreaksbr
-
+import firebase_admin
+from firebase_admin import credentials, storage
+import requests
+import fitz  
+from datetime import datetime, timedelta
+import pyshorteners
+import os
+from werkzeug.utils import secure_filename
+import PyPDF2
+import re
 # Create your views here.
 
-def n2br(value):
-    value = r_clearning(value)
-    return tuple(item.replace('\n', '<br/>') for item in value)
-
-
-def r_clearning(value):
-    return tuple(item.replace('\r', '') for item in value)
-
+cred = credentials.Certificate("/home/mohammed_shaneeb/Desktop/AI-JOB-PORTAL/firebasecrediantials.json")
+firebase_admin.initialize_app(cred, {"storageBucket": "resume-qa-4de39.appspot.com"})
+bitly_shortener = pyshorteners.Shortener(api_key="1e11476d96a20bbd9924ad6ced6ba426f90039da",timeout=10)
+api_key = 'JuFDoAhCjQj9XnLZKiJ7VwHMpoaWGUbYRNBNmLATQPQOfx2f4fnmFyT7Viqi'
 
 @login_required(login_url="signin")
 def home(request):
@@ -38,4 +43,44 @@ def submit_job(request,job_id):
         'job_description':job_description,
         'domain':domain
     }
+    if request.method == 'POST' and request.FILES.get('pdf_file'):
+        pdf_file = request.FILES['pdf_file']
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # extracted_text = ""
+        # for page_num in range(len(pdf_reader.pages)):
+        #     extracted_text += pdf_reader.pages[page_num].extract_text()
+
+        #text extraction
+        def extract_text_from_pdf(pdf_file_path):
+            text = ''
+            with fitz.open(pdf_file_path) as pdf:
+                for page_num in range(pdf.page_count):
+                    page = pdf[page_num]
+                    text += page.get_text()
+                    text = text.strip().lower()
+                    text = ' '.join(text.split())
+            return text
+        
+        # Save the PDF file to the 'resumes' folder
+        file_name = pdf_file.name
+        file_path = os.path.join('/home/mohammed_shaneeb/Desktop/AI-JOB-PORTAL/resumes', file_name)
+        with open(file_path, 'wb') as destination:
+            for chunk in pdf_file.chunks():
+                destination.write(chunk)
+
+        resume_content = extract_text_from_pdf(file_path)
+        job_description = re.sub(r'\s+|[•&]', ' ', job_description).lower()
+        job_description = job_description.replace("\\r\\n", "")
+
+
+        print('\n')
+        print("Resume Content")
+        print(resume_content)
+        print('\n')
+        print("Job Description")
+        print(job_description)
+
+       
+
     return render(request,'home/submit_job.html',{'job':job})
